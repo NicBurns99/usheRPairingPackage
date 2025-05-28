ad70ed0c95c1ebc83c25019dc6b6dc055d3b26ef
Nicola Burns <s1725659@ed.ac.uk>
2025-05-28 08:59:38 +0100

    COMMIT - NEW ADDITION - LIST CONVERSION TO DF FOR usheR package

---
+++ R/usheRpairs_DF.R
@@ -0,0 +1,32 @@
+#' Convert pairings list to data frame
+#'
+#' Converts the output of create_pairs() into a data frame with the columns Week (integer), Group (integer), and Student_Name (Character)
+#'
+#' @param pairings A list object returned from create_pairs()
+#'
+#' @returns A data frame with the columns Week, Group, and Student_Names
+#' @export
+#'
+#' @examples
+#' df <- pairings_to_df(pairings)
+#'
+pairings_to_df <- function(pairings) {
+  rows <- list() #list for accumulating rows of final df
+  
+  #Loop through list to extract each weeks data (1 to however many weeks etc)
+  for (week in seq_along(pairings)) { # seq_along() - index for each week
+    week_data <- pairings[[week]] # get groups for that week
+    
+    #Loop through each pair in each week and split students?
+    for (group in seq_along(week_data)) { # loop each group in that week
+        rows <- append(rows, list(data.frame(
+          Week = week,
+          Group = group,
+          Pair = week_data[group],
+          stringsAsFactors = F
+        )))
+      }
+    }
+  
+  do.call(rbind.data.frame, rows) #stack rows list and converts to df
+}
